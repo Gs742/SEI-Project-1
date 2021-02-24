f357f3ce70ea50cd7fc4e7d433eb9575c5331df4
georges <george.shaw2001@gmail.com>
2021-02-24 16:48:24 +0000

    push

--- SEI-Project-1/scripts/app.js
+++ SEI-Project-1/scripts/app.js
@@ -2,51 +2,71 @@ function init(){
 console.log("JAVASCRIPT CONNECTED")
 const grid = document.querySelector('.grid')
   
-  const width = 10
-  const cellCount = width * width
+  const width = 20
+  const height = 15
+  const cellCount = (width * height)
   const cells = []
 
-  const playerClass = 'player'
-  const playerStartPosition = cellCount - width/2 
-  let playerCurrentPosition = 0
+  const alien = { }
 
+  const player = {
+  playerClass: 'player',
+  playerStartPosition: cellCount - width,
+  playerCurrentPosition: 0,
+  lives: 3
+  }
 
-  createGrid(playerStartPosition)
-  addplayer(playerStartPosition)
+  const asteroid = {
 
+  }
 
-  // * Make a grid
-  function createGrid(playerStartPos) {
+  const gameBoard = createGrid()
+  loadObjects(alien, player, asteroid)
+
+  function createGrid() {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       cell.textContent = i
       grid.appendChild(cell)
       cells.push(cell)
     }
-    
+    return(cells)   
+  }
+
+  function loadObjects(alien, player, asteroid){
+  addplayer(player.playerStartPosition)
+  console.log(player)
+
   }
 
   function addplayer(position) { 
-    cells[position].classList.add(playerClass) 
-    playerCurrentPosition = position
+    gameBoard[position].classList.add(player.playerClass) 
+    player.playerCurrentPosition = position
+  }
+  function removeplayer(position) {
+    gameBoard[position].classList.remove(player.playerClass)
   }
 
-  function removeplayer(position) {
-    cells[position].classList.remove(playerClass)
+  
+
+  function addAlien(amount){
+  if(gameBoard[0].classList.alien === null){
+    gameBoard[0].classList.add(alien)
   }
+  }
+
 
 
   function handleKeyUp(event) {
     const key = event.keyCode
 
-    removeplayer(playerCurrentPosition)
+    removeplayer(player.playerCurrentPosition)
     
-    if (key === 39 && playerCurrentPosition % width !== width - 1) {
-      playerCurrentPosition++
-    } else if (key === 37 && playerCurrentPosition % width !== 0) {
-      playerCurrentPosition--
-
-    } else if (key === 38 && playerCurrentPosition >= width) {
+    if (key === 39 && player.playerCurrentPosition < gameBoard.length - 1) {
+      player.playerCurrentPosition++
+    } else if (key === 37 && player.playerCurrentPosition >= gameBoard.length - width +1 ) {
+      player.playerCurrentPosition--
+    } else if (key === 38 && player.playerCurrentPosition >= width) {
       //Up
       console.log('Up Pressed - Shoot Projectile')
     } else if (key === 40) {
@@ -55,7 +75,7 @@ const grid = document.querySelector('.grid')
       console.log('INVALID KEY')
     }
     
-    addplayer(playerCurrentPosition)
+    addplayer(player.playerCurrentPosition)
   }
 
   // * Event listeners
